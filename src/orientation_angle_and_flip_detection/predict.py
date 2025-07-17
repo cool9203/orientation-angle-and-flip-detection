9959ce3b24197e186348959c668b134252ea7308
cool9203 <octer18@gmail.com>
2025-07-17 15:59:49 +0800

    Fix: predict error logical

--- src/orientation_angle_and_flip_detection/predict.py
+++ src/orientation_angle_and_flip_detection/predict.py
@@ -5,6 +5,7 @@ import os
 import pprint
 from pathlib import Path
 
+import tqdm as TQDM
 from matplotlib import pyplot as plt
 from PIL import Image
 from transformers import AutoImageProcessor, ConvNextFeatureExtractor
@@ -18,6 +19,7 @@ def arg_parser() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Convert dataset from folder")
     parser.add_argument("-m", "--model_name_or_path", type=str, required=True, help="Model path")
     parser.add_argument("-i", "--input_paths", type=str, nargs="+", default=[], help="Input paths")
+    parser.add_argument("-ir", "--input_reference_image_path", type=str, required=True, help="Input reference image path")
     parser.add_argument("--tqdm", action="store_true", help="Show progress bar")
 
     args = parser.parse_args()
@@ -26,16 +28,20 @@ def arg_parser() -> argparse.Namespace:
 
 
 def predict_one(
-    image_path: os.PathLike,
+    image_path_reference: os.PathLike,
+    image_path_predict: os.PathLike,
     model: OAaFDNet,
     processor: ConvNextFeatureExtractor,
 ) -> Image.Image:
-    if Path(image_path).suffix not in supported_extensions:
-        raise TypeError(f"Not support image extension: '{image_path!s}'")
+    image_reference = Image.open(image_path_reference)
+    image_reference = image_reference.convert("RGB")
+    image_predict = Image.open(image_path_predict)
+    image_predict = image_predict.convert("RGB")
 
-    image = Image.open(image_path)
-    image = image.convert("RGB")
-    output: ImageOrientationAngleAndFlipOutputWithNoAttention = model(**processor(image, return_tensors="pt"))
+    output: ImageOrientationAngleAndFlipOutputWithNoAttention = model(
+        pixel_values_1=processor(image_reference, return_tensors="pt")["pixel_values"],
+        pixel_values_2=processor(image_predict, return_tensors="pt")["pixel_values"],
+    )
 
     logits_angle = output.logits_angle
     logits_flip = output.logits_flip
@@ -44,17 +50,29 @@ def predict_one(
     need_flip = True if logits_flip[1] == 1 else False
 
     for _ in range(rotate_counts):
-        image = image.rotate(90, expand=True)
-    image = image.transpose(Image.FLIP_LEFT_RIGHT) if need_flip else image
+        image_predict = image_predict.rotate(90, expand=True)
+    image_predict = image_predict.transpose(Image.FLIP_LEFT_RIGHT) if need_flip else image_predict
 
-    return image
+    return image_predict
 
 
 def predict(
     model_name_or_path: str,
     input_paths: list[os.PathLike],
+    input_reference_image_path: os.PathLike,
     tqdm: bool = True,
 ):
+    reference_images: dict[str, str | Path] = dict()
+
+    # Load reference image data
+    for reference_image_path in (
+        TQDM.tqdm(list(Path(input_reference_image_path).iterdir()), desc="Load reference images")
+        if tqdm
+        else Path(input_reference_image_path).iterdir()
+    ):
+        if reference_image_path.suffix in supported_extensions:
+            reference_images[reference_image_path.stem] = reference_image_path
+
     processor: ConvNextFeatureExtractor = AutoImageProcessor.from_pretrained(model_name_or_path)
     model = OAaFDNet.from_pretrained(
         pretrained_model_name_or_path=model_name_or_path,
@@ -62,20 +80,19 @@ def predict(
 
     for input_path in input_paths:
         if Path(input_path).is_file():
-            image = predict_one(
-                image_path=input_path,
-                model=model,
-                processor=processor,
-            )
-        else:
-            for category_folder in Path(input_path).iterdir():
-                for image_path in category_folder.iterdir():
-                    image = predict_one(
-                        image_path=image_path,
-                        model=model,
-                        processor=processor,
-                    )
-                    plt.show(image)
+            raise TypeError("Not support pass file")
+
+        for category_folder in Path(input_path).iterdir():
+            for image_path in category_folder.iterdir():
+                if image_path.suffix not in supported_extensions:
+                    continue
+                image = predict_one(
+                    image_path_reference=reference_images[category_folder.name],
+                    image_path_predict=image_path,
+                    model=model,
+                    processor=processor,
+                )
+                plt.show(image)
 
 
 if __name__ == "__main__":
